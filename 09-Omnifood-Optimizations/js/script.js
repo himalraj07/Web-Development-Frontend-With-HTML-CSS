d1f0fee4724104037cb3b17fb0fd807c85effd13
himalraj07 <himalrajbhusal@gmail.com>
2024-01-13 18:06:16 +0545

    Written a year updater code for copyright year in js

--- 09-Omnifood-Optimizations/js/script.js
+++ 09-Omnifood-Optimizations/js/script.js
@@ -1,3 +1,20 @@
+console.log("Hello world!");
+
+const myName = "Himal Raj Bhusal";
+const h1 = document.querySelector(".heading-primary");
+console.log(myName);
+console.log(h1);
+
+// h1.addEventListener("click", function () {
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
